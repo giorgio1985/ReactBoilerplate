2b2347ed984a4b81cd477a10a561d671e16626f7
giorgio1985 <g.adonoo@yahoo.it>
2020-07-16 13:04:47 +0200

    joinment parallax to porfolio component

--- app/containers/Portfolio/index.js
+++ app/containers/Portfolio/index.js
@@ -21,6 +21,18 @@ import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
 import ShowPortfolio from './ShowPortfolio';
+import hipster from '../../images/hipster.jpg';
+import template1 from '../../images/template1.jpg';
+import template2 from '../../images/template2.jpg';
+
+const inlineStyle = {
+  background: 'white',
+  left: '50%',
+  top: '50%',
+  position: 'absolute',
+  padding: '20px',
+  transform: 'translate(-50%, -50%)',
+};
 
 export function Portfolio() {
   useInjectReducer({ key: 'portfolio', reducer });
@@ -38,9 +50,25 @@ export function Portfolio() {
       </ShowPortfolio>
       <HR />
 
-      <Parallax>
-        <p>Working progress ...</p>
+      <Parallax bgImage={hipster} strength={500}>
+        <div style={{ height: 500 }}>
+          <div style={inlineStyle}>Primo progetto ...</div>
+        </div>
       </Parallax>
+      <h1 style={{ textAlign: 'center' }}> ||| </h1>
+      <Parallax bgImage={template1} strength={500}>
+        <div style={{ height: 500 }}>
+          <div style={inlineStyle}>Secondo progetto ...</div>
+        </div>
+      </Parallax>
+      <h1 style={{ textAlign: 'center' }}> ||| </h1>
+      <Parallax bgImage={template2} strength={500}>
+        <div style={{ height: 500 }}>
+          <div style={inlineStyle}>Terzo progetto ...</div>
+        </div>
+      </Parallax>
+
+      <div style={{ height: '50vh' }} />
     </div>
   );
 }
